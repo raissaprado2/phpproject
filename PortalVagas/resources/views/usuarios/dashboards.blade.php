93419a50ab6b6c754d5bfbb42fd6ca8bc7dd910a
raissaprado2 <ra.pradoviturino@gmail.com>
2024-08-13 15:11:29 -0300

    13/08

---
+++ PortalVagas/resources/views/usuarios/dashboards.blade.php
@@ -0,0 +1,7 @@
+div <div class="container">
+    <h2>Dashboard - Usuário</h2>
+    <form action="{{route(usuarios.logout)}}" method="post">
+        <input type="submit" value="Sair">
+    </form>
+</div>
+
